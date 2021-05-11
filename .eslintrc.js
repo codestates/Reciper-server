bf0e4420d3034a2a0f8c55c70144e6417db61a8e
katsukichi <nsg8957@naver.com>
2021-05-11 14:18:01 +0900

    Fix #5 ormconfig file type changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,6 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
 	},
 	rules: {
 		'no-empty': 'error', // 빈 블록문을 지양한다.
@@ -14,6 +11,7 @@ module.exports = {
 		'default-case-last': 'error', // switch 문에 default가 항상 마지막에 작성되어야 한다.
 		eqeqeq: ['error', 'always'], // '==' 밎 '!=' 사용을 지양한다.
 		'prefer-template': 'error', // 문자열 연결 연산자를 지양하고 템플릿 리터럴을 사용한다.
+		'no-var-requires': true,
 	},
 	settings: {},
 };
---
+++ ormconfig.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+	type: 'mysql',
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	username: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+	synchronize: true,
+	logging: false,
+	entities: ['src/entity/**/*.ts'],
+	migrations: ['src/migration/**/*.ts'],
+	subscribers: ['src/subscriber/**/*.ts'],
+	cli: {
+		entitiesDir: 'src/entity',
+		migrationsDir: 'src/migration',
+		subscribersDir: 'src/subscriber',
+	},
+};
