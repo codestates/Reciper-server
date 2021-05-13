958f526dfad2957a860bbefd0d3aeceaa961f9a8
Jung SongYi <lovelysi0113@gmail.com>
2021-05-13 11:33:55 +0900

    Add #33 controller delete /recruitBoardComment/:board_id/:comment_id logic

--- controllers/recruit/deleteComment.ts
+++ controllers/recruit/deleteComment.ts
@@ -1,8 +1,67 @@
 import { Request, Response } from 'express';
+import { Recruits } from './../../src/entity/Recruits';
+import { getRepository } from 'typeorm';
+import { Recruit_comments } from './../../src/entity/Recruit_comments';
 
 const deleteComment = async (req: Request, res: Response) => {
 	// 댓글 삭제
-	console.log('💜deleteComment- ', req.body);
+	console.log('💜deleteComment- ', req.body, req.params);
+	const boardId = Number(req.params.board_id);
+	const commentId = Number(req.params.comment_id);
+	// 해당 게시글 찾기
+	let boardInfo;
+	try {
+		boardInfo = await Recruits.findOne({
+			id: boardId,
+		});
+	} catch (err) {
+		console.log('💜showRecruitBoard- err: ', err.message);
+	}
+	if (boardInfo === undefined) {
+		res.status(400).json({
+			message: 'no data about board ' + boardId,
+		});
+	} else {
+		// 게시글에서 해당 댓글 지우기
+		const commentData = boardInfo.recruit_commentId;
+		for (let idx = 0; idx < commentData.length; idx++) {
+			if (commentData[idx].id === commentId) {
+				commentData.splice(idx, 1);
+				break;
+			}
+		}
+		try {
+			boardInfo.save();
+		} catch (err) {
+			console.log('💜showRecruitBoard- err: ', err.message);
+		}
+		// 데이터베이스에서 해당 댓글 지우기
+		try {
+			await Recruit_comments.delete({
+				id: commentId,
+			});
+		} catch (err) {
+			console.log('💜showRecruitBoard- err: ', err.message);
+		}
+		// 지운 이후의 댓글 데이터 보내주기
+		let commentsAll: any[] = [];
+		try {
+			let findComments = await getRepository(Recruit_comments).findAndCount({
+				relations: ['recruits'],
+			});
+			findComments.forEach(el => {
+				if (typeof el !== 'number') {
+					commentsAll.push(el);
+				}
+			});
+		} catch (err) {
+			console.log('💜showRecruitBoard- err: ', err.message);
+		}
+		res.status(200).json({
+			...boardInfo,
+			commentsAll,
+		});
+	}
 };
 
 export default deleteComment;
