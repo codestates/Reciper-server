077081b71d7bbf94916df2b1215fa8b4da754885
Jung SongYi <lovelysi0113@gmail.com>
2021-05-13 22:18:15 +0900

    Add #15 #16 response data and database about stacks

--- controllers/profile/getProfile.ts
+++ controllers/profile/getProfile.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { Users } from '../../src/entity/Users';
+import { getRepository } from 'typeorm';
 
 const getProfile = async (req: Request, res: Response) => {
 	// 프로필 정보 조회
 	console.log('🧡getProfile- ', req.body);
-	const userId = req.userId;
+	const userId = 3; //req.userId;
 	// 저장된 유저 정보 불러오기
 	const userInfo = await Users.findOne({
 		id: userId,
@@ -14,12 +15,28 @@ const getProfile = async (req: Request, res: Response) => {
 			message: 'no data about profile',
 		});
 	} else {
+		// stack 데이터 가져오기
+		const stackArray: any = [];
+		const stackData = await getRepository(Users).find({
+			relations: ['join'],
+			where: {
+				id: userInfo.id,
+			},
+		});
+		console.log(stackData); // test
+		stackData.map(el => {
+			el.join.map(e => {
+				stackArray.push(e.name);
+			});
+		});
+		// 형태 변환
 		if (userInfo.career !== '') {
 			userInfo.career = JSON.parse(userInfo.career);
 		}
-		console.log(userInfo);
+		console.log(userInfo, stackArray); // test
 		res.status(200).json({
 			...userInfo,
+			stacks: stackArray,
 		});
 	}
 };
