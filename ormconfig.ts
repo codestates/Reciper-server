c675bff50ebface9c4c3d08d95e5f3cd9aceb909
Jung SongYi <lovelysi0113@gmail.com>
2021-05-15 02:05:30 +0900

    Fix option setting

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,7 +8,7 @@ export default {
 	username: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
-	synchronize: false,
+	synchronize: true,
 	logging: false,
 	entities: ['src/entity/**/*.ts'],
 	migrations: ['src/migration/**/*.ts'],
