b79d6472451fbbcb3c25d715763532052426a725
Jung SongYi <lovelysi0113@gmail.com>
2021-05-15 02:29:40 +0900

    Rename #17 request value from profileImageName to uploadImageName

--- src/customType/express.d.ts
+++ src/customType/express.d.ts
@@ -4,7 +4,7 @@ export declare global {
 			newAccessToken?: string | undefined;
 			userId?: number | undefined;
 			userEmail?: string | undefined;
-			profileImageName?: string | undefined;
+			uploadImageName?: string | undefined;
 		}
 	}
 }
