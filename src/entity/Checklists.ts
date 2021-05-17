a9de08080731434fe869dc3e3b7ec034ce5e709a
Jung SongYi <lovelysi0113@gmail.com>
2021-05-17 15:38:51 +0900

    Add #71 entity for create Checklists table

---
+++ src/entity/Checklists.ts
@@ -0,0 +1,31 @@
+import {
+	Entity,
+	BaseEntity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn,
+	ManyToOne,
+} from 'typeorm';
+import { Tasks } from './Tasks';
+
+@Entity()
+export class Checklists extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id!: number;
+
+	@Column()
+	desc!: string;
+
+	@Column()
+	isChecked!: boolean;
+
+	@CreateDateColumn({ name: 'createdAt' })
+	createdAt!: Date;
+
+	@UpdateDateColumn({ name: 'updatedAt' })
+	updatedAt!: Date;
+
+	@ManyToOne(type => Tasks, task => task.checklists)
+	task!: Tasks;
+}
