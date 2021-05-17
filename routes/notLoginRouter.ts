5cc2b825ada0eb100359f838e777e13f1db6dea6
Jung SongYi <lovelysi0113@gmail.com>
2021-05-17 14:29:23 +0900

    Add #62 sort value in req.params for sorting boardList

--- routes/notLoginRouter.ts
+++ routes/notLoginRouter.ts
@@ -5,10 +5,10 @@ const notLoginRouter = express.Router();
 // stack 정보 조회
 notLoginRouter.get('/stacks', controller.getStacks);
 // 팀원모집 게시글 리스트 조회
-notLoginRouter.get('/recruitList/:order', controller.recruitList);
+notLoginRouter.get('/recruitList/:order/:sort', controller.recruitList);
 // 팀원모집 게시글 상세내용 조회
 notLoginRouter.get('/recruitBoard/:board_id', controller.showRecruitBoard);
-// 팀원모집 게시글 리스트 검색
-notLoginRouter.post('/filterRecruitList', controller.filterRecruitList);
+// 팀원모집 게시글 리스트 검색 - stacks으로 검색
+notLoginRouter.post('/filterRecruitList/:order/:sort', controller.filterRecruitList);
 
 export default notLoginRouter;
