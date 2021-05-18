a52c7ec6dc54691475db7d352efd8f0e3d2587e1
Jung SongYi <lovelysi0113@gmail.com>
2021-05-19 00:06:06 +0900

    Add #84 controller get /project logic

--- controllers/project/showProjectList.ts
+++ controllers/project/showProjectList.ts
@@ -1,9 +1,41 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Users } from '../../src/entity/Users';
+import { Projects } from '../../src/entity/Projects';
 
 const showProjectList = async (req: Request, res: Response) => {
 	// 프로젝트 리스트 조회
 	console.log('💛showProjectList- ');
 	console.log(req.body, req.params);
+	const userId = req.userId;
+	// 유저 정보로 프로젝트 리스트 찾기
+	try {
+		const userInfo = await Users.findOne({
+			where: {
+				id: userId,
+			},
+		});
+		const projectList = await getRepository(Projects).find({
+			where: {
+				members: {
+					id: userId,
+				},
+			},
+			order: {
+				createdAt: 'DESC', // 순서 고민해보기
+			},
+		});
+		console.log(userInfo, projectList); //test
+		res.status(200).json({
+			...userInfo,
+			projectList,
+		});
+	} catch (err) {
+		console.log('💛showProjectList- err: ', err.message);
+		res.status(400).json({
+			message: err.message,
+		});
+	}
 };
 
 export default showProjectList;
