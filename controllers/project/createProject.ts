81f2ba7bfbdfaae0973d9e0ddedca4f48f048187
Jung SongYi <lovelysi0113@gmail.com>
2021-05-19 10:34:10 +0900

    Add #88 controller post /project logic

--- controllers/project/createProject.ts
+++ controllers/project/createProject.ts
@@ -1,9 +1,40 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Users } from '../../src/entity/Users';
+import { Projects } from '../../src/entity/Projects';
 
 const createProject = async (req: Request, res: Response) => {
 	// 프로젝트 생성
 	console.log('💛createProject- ');
 	console.log(req.body, req.params);
+	const userId = req.userId;
+	const { name, projectURL } = req.body;
+	// 유저 정보 가져오기
+	const userInfo = await Users.findOne({
+		id: userId,
+	});
+	if (userInfo) {
+		// 새로운 프로젝트 정보 저장
+		const created = await Projects.create({
+			name,
+			projectURL,
+		});
+		const membersArray = [userInfo];
+		created.members = membersArray;
+		try {
+			await created.save();
+			console.log(created); // test
+			res.status(200).json({
+				...created,
+			});
+		} catch (err) {
+			// 만약 projectURL에 중복되는 value를 저장하려고 하면 에러 발생(QueryFailedError: Duplicate entry)
+			console.log('💛createProject- err: ', err.message);
+			res.status(400).json({
+				message: err.message,
+			});
+		}
+	}
 };
 
 export default createProject;
