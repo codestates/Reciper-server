7521f6345537319c3b3da70aba1adca067505566
Jung SongYi <lovelysi0113@gmail.com>
2021-05-21 20:07:00 +0900

    Add #70 memberChecker before /project/:projectURL and /projectInvite router

--- routes/project.ts
+++ routes/project.ts
@@ -1,13 +1,19 @@
 import * as controller from '../controllers/controller';
 import express from 'express';
 import authChecker from '../middlewares/authChecker';
+import memberChecker from '../middlewares/memberChecker';
 const projectRouter = express.Router();
 
 projectRouter.use('/project', (req, res, next) => {
 	authChecker(req, res, next);
 });
+projectRouter.use('/project/:projectURL', (req, res, next) => {
+	authChecker(req, res, next);
+	memberChecker(req, res, next);
+});
 projectRouter.use('/projectInvite', (req, res, next) => {
 	authChecker(req, res, next);
+	memberChecker(req, res, next);
 });
 
 // 프로젝트 리스트 조회
