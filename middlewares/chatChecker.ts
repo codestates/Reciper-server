1a23f2d6c8e7ef4f3783d83a1e38c96bc879c4ef
Jung SongYi <lovelysi0113@gmail.com>
2021-05-24 19:09:40 +0900

    Add #127 middleware - chatChecker logic

---
+++ middlewares/chatChecker.ts
@@ -0,0 +1,40 @@
+import { Socket } from '../node_modules/socket.io/dist/socket';
+import { Projects } from '../src/entity/Projects';
+
+const chatChecker = async (socket: Socket, next: Function) => {
+	console.log('🔏chatChecker 실행합니다- \n', socket.handshake.auth, '\n', socket.handshake.query);
+	const { projectURL } = socket.handshake.query;
+	const { token, logintype } = socket.handshake.auth;
+	const userId = 11; // 하드코딩
+	try {
+		const foundProject = await Projects.findOne({
+			relations: ['members'],
+			where: {
+				projectURL,
+			},
+		});
+		if (foundProject) {
+			const chkMembers = foundProject.members.map(el => el.id);
+			console.log('🔏', projectURL, ' member: ', chkMembers); // test
+			if (chkMembers.includes(userId)) {
+				console.log('🔏chatChecker 결과- ', userId, 'is member in', projectURL);
+				socket.handshake.query.projectId = String(foundProject.id);
+				socket.handshake.query.userId = String(userId);
+				// 실제 요청으로 넘어감
+				console.log('🔏go next function!!\n');
+				next();
+			} else {
+				console.log('🔏chatChecker- err: ', userId, ' is not member in ', projectURL);
+				next(new Error());
+			}
+		} else {
+			console.log('🔏chatChecker- err: ', projectURL, ' project is not found');
+			next(new Error());
+		}
+	} catch (err) {
+		console.log('🔏chatChecker- err: ', err.message);
+		next(new Error());
+	}
+};
+
+export default chatChecker;
