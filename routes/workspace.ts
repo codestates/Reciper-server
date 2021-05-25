8b48642a81dc678b0fa616fe59eaa53d2970b3ad
Jung SongYi <lovelysi0113@gmail.com>
2021-05-25 15:02:34 +0900

    Fix #123 route path and parameters about chat controllers

--- routes/workspace.ts
+++ routes/workspace.ts
@@ -5,16 +5,16 @@ import memberChecker from '../middlewares/memberChecker';
 const workspaceRouter = express.Router();
 
 // middleware
-workspaceRouter.use('/workspace', authChecker, memberChecker);
+workspaceRouter.use('/workspace/:projectURL', authChecker, memberChecker);
 
 // 6. chat(채팅)
 // 채팅방 목록 조회
-workspaceRouter.get('/workspace/chat/:projectURL', controller.showChatRooms);
+workspaceRouter.get('/workspace/:projectURL/chat', controller.showChatRooms);
 // 채팅방 생성
-workspaceRouter.post('/workspace/chat/:projectURL', controller.createChatRoom);
+workspaceRouter.post('/workspace/:projectURL/chat', controller.createChatRoom);
 // 채팅방 이름 수정
-workspaceRouter.post('/workspace/chat/:projectURL/:room', controller.editChatRoom);
+workspaceRouter.post('/workspace/:projectURL/chat/:room', controller.editChatRoom);
 // 채팅방 삭제
-workspaceRouter.delete('/workspace/chat/:projectURL/:room', controller.deleteChatRoom);
+workspaceRouter.delete('/workspace/:projectURL/chat/:room', controller.deleteChatRoom);
 
 export default workspaceRouter;
