7b81ef5dc56accd880ef6bfc7728a756c5eff9b9
Jung SongYi <lovelysi0113@gmail.com>
2021-05-25 15:34:33 +0900

    Add #140 controller get /workspace/:projectURL/chat logic

--- controllers/workspace/showChatRooms.ts
+++ controllers/workspace/showChatRooms.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Rooms } from '../../src/entity/Rooms';
 
 const showChatRooms = async (req: Request, res: Response) => {
 	// 채팅방 목록 조회
 	console.log('💚showChatRooms-', req.body, req.params);
+	const { projectURL } = req.params;
+	const allChatRooms = await getRepository(Rooms).find({
+		relations: ['project'],
+		where: {
+			project: {
+				projectURL,
+			},
+		},
+	});
+	let roomsList = [];
+	for (let idx = 0; idx < allChatRooms.length; idx++) {
+		let obj = { name: allChatRooms[idx].name };
+		roomsList.push(obj);
+	}
+	console.log('💚showChatRooms-result:', roomsList); // test
+	res.status(200).json({
+		roomsList,
+	});
 };
 
 export default showChatRooms;
