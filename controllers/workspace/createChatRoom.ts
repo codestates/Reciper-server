e4b8c5b0327b80e265bbab83f4172faac376a7dc
Jung SongYi <lovelysi0113@gmail.com>
2021-05-25 14:56:55 +0900

    Add #141 controller post /workspace/chat/:projectURL logic

--- controllers/workspace/createChatRoom.ts
+++ controllers/workspace/createChatRoom.ts
@@ -1,8 +1,30 @@
 import { Request, Response } from 'express';
+import { Projects } from '../../src/entity/Projects';
+import { Rooms } from '../../src/entity/Rooms';
 
 const createChatRoom = async (req: Request, res: Response) => {
 	// 채팅방 생성
 	console.log('💚createChatRoom-', req.body, req.params);
+	const { name } = req.body;
+	const { projectURL } = req.params;
+	// 프로젝트 정보 가져오기
+	const foundProject = await Projects.findOne({
+		where: {
+			projectURL,
+		},
+	});
+	if (foundProject) {
+		// 새로운 채팅방 생성
+		let newRoom = await Rooms.create({
+			name,
+			project: foundProject,
+		});
+		await newRoom.save();
+		console.log('💚createChatRoom-result:', newRoom);
+		res.status(200).json({
+			...newRoom,
+		});
+	}
 };
 
 export default createChatRoom;
