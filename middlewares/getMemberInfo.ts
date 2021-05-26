2a4f8d2efbb5f1183bf5177d390aca08e7390a96
Jung SongYi <lovelysi0113@gmail.com>
2021-05-26 17:31:23 +0900

    Add #154 middleware - getMemberInfo logic

---
+++ middlewares/getMemberInfo.ts
@@ -0,0 +1,36 @@
+import { Projects } from '../src/entity/Projects';
+
+const getMemberInfo = async (userId: number, projectURL: string | string[]) => {
+	console.log('🔎getMemberInfo-\n', { userId, projectURL });
+	const result = {
+		projectId: -1,
+	};
+	try {
+		const foundProject = await Projects.findOne({
+			relations: ['members'],
+			where: {
+				projectURL,
+			},
+		});
+		if (foundProject) {
+			const chkMembers = foundProject.members.map(el => el.id);
+			console.log('🔎getMemberInfo-chk:', projectURL, 'member:', chkMembers);
+			if (chkMembers.includes(userId)) {
+				console.log('🔎getMemberInfo-result:', userId, 'is member in', projectURL);
+				result.projectId = foundProject.id;
+			} else {
+				console.log('🔎getMemberInfo-err:', userId, 'is not member in', projectURL);
+				throw new Error(userId + ' is not member in ' + projectURL);
+			}
+		} else {
+			console.log('🔎getMemberInfo-err:', projectURL, 'project is not found');
+			throw new Error(projectURL + ' project is not found');
+		}
+	} catch (err) {
+		console.log('🔎getMemberInfo-err:', err.message);
+		throw new Error(err);
+	}
+	return result;
+};
+
+export default getMemberInfo;
