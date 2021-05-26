91db8448bee025ce97bce71a9f136711ae0d35a6
katsukichi <nsg8957@naver.com>
2021-05-26 23:44:22 +0900

    Add #136 kanban board socket implement with 8 event

--- ormconfig.ts
+++ ormconfig.ts
@@ -8,7 +8,7 @@ export default {
 	username: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
 	database: process.env.DATABASE_NAME,
-	synchronize: true,
+	synchronize: false,
 	logging: false,
 	entities: ['src/entity/**/*.ts'],
 	migrations: ['src/migration/**/*.ts'],
