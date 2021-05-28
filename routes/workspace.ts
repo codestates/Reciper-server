bbd0bfa9d1dd487f5f94e6e4488fb6ebbecb0293
Jung SongYi <lovelysi0113@gmail.com>
2021-05-28 17:44:50 +0900

    Add #169 route about workspace feature for kanban

--- routes/workspace.ts
+++ routes/workspace.ts
@@ -7,7 +7,17 @@ const workspaceRouter = express.Router();
 // middleware
 workspaceRouter.use('/workspace/:projectURL', authChecker, projectChecker);
 
-// 6. chat(채팅)
+// 5-1. tasks(칸반보드, 캘린더)
+// part 목록 조회
+workspaceRouter.get('/workspace/:projectURL/kanban', controller.showKanbanParts);
+// part 생성
+workspaceRouter.post('/workspace/:projectURL/kanban', controller.createKanbanPart);
+// part 이름 수정
+workspaceRouter.post('/workspace/:projectURL/kanban/:part', controller.editKanbanPart);
+// part 삭제
+workspaceRouter.delete('/workspace/:projectURL/kanban/:part', controller.deleteKanbanPart);
+
+// 5-2. chat(채팅)
 // 채팅방 목록 조회
 workspaceRouter.get('/workspace/:projectURL/chat', controller.showChatRooms);
 // 채팅방 생성
