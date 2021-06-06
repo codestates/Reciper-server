e1883c566f1b13eb40c480c634e1643ad62fb217
katsukichi <nsg8957@naver.com>
2021-06-07 01:47:27 +0900

    Refactor #233 webRTC refactoring

---
+++ controllers/workspace/socketWebRTC.ts
@@ -0,0 +1,46 @@
+import { Socket } from 'socket.io/dist/socket';
+import app from '../../app';
+
+const users: any = {};
+const socketToRoom: any = {};
+
+const socketWebRTC = async (socket: Socket) => {
+	// const webRTCIo = app.get('webRTCIo');
+	// console.log(socket);
+	socket.on('join room', roomID => {
+		console.log('조인!!!', roomID);
+		if (users[roomID]) {
+			const length = users[roomID].length;
+			if (length === 4) {
+				socket.emit('room full');
+				return;
+			}
+			users[roomID].push(socket.id);
+		} else {
+			users[roomID] = [socket.id];
+		}
+		socketToRoom[socket.id] = roomID;
+		const usersInThisRoom = users[roomID].filter((id: string) => id !== socket.id);
+
+		socket.emit('all users', usersInThisRoom);
+	});
+
+	socket.on('sending signal', payload => {
+		socket.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+	});
+
+	socket.on('returning signal', payload => {
+		socket.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+	});
+
+	socket.on('disconnect', () => {
+		const roomID = socketToRoom[socket.id];
+		let room = users[roomID];
+		if (room) {
+			room = room.filter((id: string) => id !== socket.id);
+			users[roomID] = room;
+		}
+	});
+};
+
+export default socketWebRTC;
